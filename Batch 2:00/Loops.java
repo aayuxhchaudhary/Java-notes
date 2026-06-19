67eeffe005d8577be81083fb14ffcc9e267d43f1
Aayush <aayushc951@gmail.com>
2026-06-19 15:18:06 +0530

    Day 1

---
+++ Batch 2:00/Loops.java
@@ -0,0 +1,32 @@
+// // For loop
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.println(2*i);
+//         }
+//     }
+// }
+
+
+// // While loop
+// public class WhileLoop{
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while (i < 5) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+// // do-while
+// public class DoWhileLoop {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         do {
+//             System.out.println(i);
+//         } while (i>0);
+//     }
+// }
+
+
